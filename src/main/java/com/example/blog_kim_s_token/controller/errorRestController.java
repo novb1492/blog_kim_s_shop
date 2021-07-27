b6668a9823234162c676c1bd116b5e0ba4ed35dd
novb1492 <novb1492@naver.com>
2021-07-27 13:57:34 +0900

    ++

--- src/main/java/com/example/blog_kim_s_token/controller/errorRestController.java
+++ src/main/java/com/example/blog_kim_s_token/controller/errorRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class errorController {
+public class errorRestController {
     @Autowired
     private utillService utillService;
     
