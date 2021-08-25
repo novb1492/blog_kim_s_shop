3504d9432fd2c7073a42aefc0332721f191cc653
novb1492 <novb1492@naver.com>
2021-08-25 11:11:22 +0900

    ++

--- src/main/java/com/example/blog_kim_s_token/service/priceService.java
+++ src/main/java/com/example/blog_kim_s_token/service/priceService.java
@@ -11,7 +11,10 @@ public class priceService {
     public JSONObject getTotalSeatPrice(seatPriceDto seatPriceDto) {
         System.out.println("getTotalSeatPrice");
         System.out.println(seatPriceDto.getTimes().size()+"선택 자리수 ");
-        return utillService.makeJson(true,getTotalSeatPrice(seatPriceDto.getSeat(), seatPriceDto.getTimes().size()));
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("totalPrice", getTotalSeatPrice(seatPriceDto.getSeat(), seatPriceDto.getTimes().size()));
+        jsonObject.put("price", 500);
+        return jsonObject;
     }
     public int getTotalSeatPrice(String seat, int timesSize) {
         System.out.println("getTotalSeatPrice");
